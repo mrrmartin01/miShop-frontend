171c25376448de81012d831f58410b662c347fad
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-03-20 20:45:38 +0000

    hero section added successfully

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,26 +1,27 @@
 import './Hero.scss'
 import hand from '../assets/wave.png'
+import hero_img from '../assets/Tommy-Hilfiger_model.png'
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero--left">
+    <div className="hero" id='hero'>
+      <div className="hero--left" id='hero-left'>
         <h2>New arrivals only</h2>
-        <div className="">
-            <div className="hand--icon">
+            <div className="hand--icon" id='hand-icon'>
                 <p>new</p>
                 <img src={hand} alt="" />
             </div>
             <p>collection</p>
             <p>for everyone</p>
-        </div>
-        <div className="hero--latest">
+        <div className="hero--latest" id='hero-latest'>
             <div className="">Latest collection</div>
-            <span> arrow</span>
+            <span> <IoIosArrowDroprightCircle/> </span>
         </div>
       </div>
-      <div className="hero--right">
-        <img src="" alt="" />
+      <div className="hero--right" id='hero-right'>
+        <img src={hero_img} alt="" />
       </div>
     </div>
   )
---
+++ src/components/items/Items.jsx
@@ -0,0 +1,10 @@
+import './Items.scss'
+const items = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default items
