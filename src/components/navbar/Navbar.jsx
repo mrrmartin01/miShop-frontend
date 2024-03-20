ef8bfa7ffc7ae3540fa8510772d20ceb85b053bf
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-03-20 14:21:47 +0000

    hamburger added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,15 +7,26 @@ import logo from '../assets/smiley.png'
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop");
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleMenuToggle = () => {
+      setShowMenu(!showMenu);
+    };
+
+    const closeToggle = () => {
+      setShowMenu(!handleMenuToggle)
+    }
 
   return (
+    <>
     <div className='navbar'>
-      <div className="nav--logo"> 
+      <div className="nav--logo" id="nav-logo"> 
         <img src={logo} alt="logo" />
         <p>miShop</p>
       </div>
       
-      <ul className='nav--menu'>
+
+      <ul className='nav--menu' id="nav-menu">
         <li onClick={() => {setMenu("shop")}}>
             <Link to={'/'}>Shop</Link> {menu === "shop"? <hr/> : <></>}
         </li>
@@ -30,7 +41,7 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <div className="nav--login--cart">
+      <div className="nav--login--cart" id="nav-login-cart">
         <Link to={'/login'}>
             <button>Login</button>
         </Link>
@@ -38,8 +49,39 @@ const Navbar = () => {
         <span><IoCart /></span>
         </Link>
         <div className="nav--cart--count">0</div>
+        <div className="hamburger" onClick={handleMenuToggle}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
+
+      <div className={`flex-nav${showMenu ? " show" : ""}`}>
+      <ul className='nav--flex' id="nav-flex">
+        <div className = 'nav--close'>
+      <span onClick={closeToggle}>x</span>
+        </div>
+        <li onClick={() => {setMenu("shop")}}>
+            <Link to={'/'}>Shop</Link> {menu === "shop"? <hr/> : <></>}
+        </li>
+        <li onClick={() => {setMenu("electronics")}}>
+            <Link to={'/electronics'}>Electronics</Link> {menu === "electronics"? <hr/> : <></>}
+        </li>
+        <li onClick={() => {setMenu("fashion")}}>
+           <Link to={'fashion'}>Fashion</Link>  {menu === "fashion"? <hr/> : <></>}
+        </li>
+        <li onClick={() => {setMenu("groceries")}}>
+            <Link to={'/groceries'}>Groceries</Link> {menu === "groceries"? <hr/> : <></>}
+        </li>
+        <div className="btn">
+        <Link to={'/login'}>
+            <button>Login</button>
+        </Link>
+        </div>
+      </ul>
+    </div>
+</>
   )
 }
 
