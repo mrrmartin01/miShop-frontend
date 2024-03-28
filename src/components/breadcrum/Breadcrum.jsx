a51438708fd2c82cb6f8dff3d75350f0c1370d85
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-03-28 19:37:37 +0000

    props for breadcrum

--- src/components/breadcrum/Breadcrum.jsx
+++ src/components/breadcrum/Breadcrum.jsx
@@ -1,13 +1,20 @@
+import PropTypes from "prop-types"
 import "./Breadcrum.scss";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
 
 const Breadcrum = (props) => {
   const { product } = props; // Corrected destructuring
   return (
     <div className="breadcrum">
-      HOME <span></span> SHOP <span></span>
-      {product && product.category} <span></span> {product && product.name}
+      HOME <span><MdOutlineKeyboardArrowRight/></span> SHOP <span><MdOutlineKeyboardArrowRight/></span>
+      {product && product.category} <span><MdOutlineKeyboardArrowRight/></span> {product && product.name}
     </div>
   );
 };
+
+Breadcrum.propTypes = {
+  product: PropTypes.any.isRequired,
+}
 
 export default Breadcrum;
